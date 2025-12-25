9d199eefba1e0e30299077d7ea274a62d29b444e
probin-dhakal <154975813+probin-dhakal@users.noreply.github.com>
2025-12-25 10:43:33 +0530

    Update job_thread.cpp

--- job_thread.cpp
+++ job_thread.cpp
@@ -1,105 +1,79 @@
-#include <queue>
-#include <string>
-#include <mutex>
-#include <condition_variable>
-#include <thread>
-#include <vector>
+#include "job_thread.h"
+#include "encryptor.h"
+#include "config.h"
+
 #include <iostream>
-using namespace std;
 
-#include "encryptor.cpp" // include Encryptor class
+std::mutex coutMutex;
 
-// ==================================================
-// Job structure
-// ==================================================
-struct Job
+void JobQueue::push(const Job &job)
 {
-    string inputFile;
-    string outputFile;
-};
-
-// ==================================================
-// Thread-safe JobQueue
-// ==================================================
-class JobQueue
-{
-public:
-    void push(const Job &job)
     {
+        std::lock_guard<std::mutex> lock(mtx_);
+        queue_.push(job);
+    }
+    cv_.notify_one();
+}
+
+bool JobQueue::pop(Job &job)
+{
+    std::unique_lock<std::mutex> lock(mtx_);
+    cv_.wait(lock, [&]
+             { return !queue_.empty() || finished_; });
+
+    if (queue_.empty())
+        return false;
+
+    job = queue_.front();
+    queue_.pop();
+    return true;
+}
+
+void JobQueue::setFinished()
+{
+    {
+        std::lock_guard<std::mutex> lock(mtx_);
+        finished_ = true;
+    }
+    cv_.notify_all();
+}
+
+ThreadPool::ThreadPool(int threads, JobQueue &queue, const std::string &key)
+    : numThreads_(threads), queue_(queue), key_(key) {}
+
+void ThreadPool::start()
+{
+    for (int i = 0; i < numThreads_; ++i)
+        workers_.emplace_back(&ThreadPool::worker, this, i);
+}
+
+void ThreadPool::join()
+{
+    for (auto &t : workers_)
+        t.join();
+}
+
+void ThreadPool::worker(int id)
+{
+    Encryptor enc(key_);
+    Job job;
+
+    while (queue_.pop(job))
+    {
+        if (verbose)
         {
-            lock_guard<mutex> lock(mtx_);
-            queue_.push(job);
+            std::lock_guard<std::mutex> lock(coutMutex);
+            std::cout << "[Thread " << id + 1 << "] Encrypting "
+                      << job.inputFile << "\n";
         }
-        cv_.notify_one();
-    }
 
-    bool pop(Job &job)
-    {
-        unique_lock<mutex> lock(mtx_);
-        while (queue_.empty() && !finished_)
-            cv_.wait(lock); // wait until either queue has a job or finished_ is true
+        enc.encryptFile(job.inputFile, job.outputFile);
 
-        if (queue_.empty())
-            return false; // finished and no jobs left
-
-        job = queue_.front();
-        queue_.pop();
-        return true;
-    }
-
-    void setFinished()
-    {
+        if (verbose)
         {
-            lock_guard<mutex> lock(mtx_);
-            finished_ = true;
-        }
-        cv_.notify_all();
-    }
-
-private:
-    queue<Job> queue_;
-    mutex mtx_;
-    condition_variable cv_;
-    bool finished_ = false;
-};
-
-// ==================================================
-// ThreadPool for concurrent encryption
-// ==================================================
-class ThreadPool
-{
-public:
-    ThreadPool(int numThreads, JobQueue &queue, const string &key)
-        : numThreads_(numThreads), queue_(queue), key_(key) {}
-
-    void start()
-    {
-        for (int i = 0; i < numThreads_; ++i)
-            workers_.emplace_back(&ThreadPool::worker, this, i);
-    }
-
-    void join()
-    {
-        for (auto &t : workers_)
-            if (t.joinable())
-                t.join();
-    }
-
-private:
-    void worker(int id)
-    {
-        Encryptor enc(key_);
-        Job job;
-        while (queue_.pop(job))
-        {
-            cout << "[Thread " << id + 1 << "] Encrypting "
-                 << job.inputFile << " -> " << job.outputFile << endl;
-            enc.encryptFile(job.inputFile, job.outputFile);
+            std::lock_guard<std::mutex> lock(coutMutex);
+            std::cout << "[Thread " << id + 1 << "] Finished "
+                      << job.outputFile << "\n";
         }
     }
-
-    int numThreads_;
-    JobQueue &queue_;
-    vector<thread> workers_;
-    string key_;
-};
+}
